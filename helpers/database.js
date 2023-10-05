a4a062cb8e335e9da40566fb4e8e7c3cff7a7c74
Salmaan Saeed <salsaeed999@gmail.com>
2023-10-05 18:50:36 -0400

    removed commented old code

--- helpers/database.js
+++ helpers/database.js
@@ -3,15 +3,6 @@ var { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 var { DynamoDB } = require("@aws-sdk/client-dynamodb");
 require('dotenv').config();
 
-/*
-AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: process.env.REGION,
-    endpoint: process.env.ENDPOINT,
-})
-*/
-
 const dynamodb = new DynamoDB({
     region: process.env.REGION,
     endpoint: process.env.ENDPOINT,
